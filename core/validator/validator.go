0fa9d2eb9347c4568ccfdb16fb7138d81e7779f0
priyank patel <priyankpatel9413@gmail.com>
2024-05-05 17:52:26 +0530

    Added Validator functionality

---
+++ core/validator/validator.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	t "github.com/malay44/chadChain/core/types"
+)
+
+func ValidateTransaction(tr t.Transaction) bool {
+	// validate transaction
+	return true
+}
+
+func ValidateBlock(b t.Block) bool {
+	// validate block
+	return true
+}
