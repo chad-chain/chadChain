8e3e6c6894ec5fc1657d92d6d04d99f251d05a3a
malay44 <malay.p1@ahduni.edu.in>
2024-05-07 23:37:34 +0530

    Add key generation functionality to initialize package

--- core/initialize/init.go
+++ core/initialize/init.go
@@ -1,9 +1,14 @@
 package initialize
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/malay44/chadChain/core/crypto"
 	db "github.com/malay44/chadChain/core/storage"
 	t "github.com/malay44/chadChain/core/types"
-	"log"
 )
 
 func GlobalDBVar() {
@@ -20,5 +25,39 @@ func GlobalDBVar() {
 		log.Default().Println(err.Error())
 	} else {
 		log.Default().Printf("LatestBlock: %x\n", t.LatestBlock)
+	}
+}
+
+func Keys() {
+	godotenv.Load(".env")
+	if os.Getenv("PRIV_HEX") == "" {
+		// create a new private key
+		_, hex, Addr, err := crypto.GenerateNewPrivateKey()
+		if err != nil {
+			log.Fatalln("Failed to generate private key")
+			return
+		}
+		os.Setenv("PRIV_HEX", hex)
+		os.Setenv("WALLET_ADDR", Addr.Hex())
+		log.Default().Println("Private key and Address generated")
+		log.Default().Println("Save the following in your env file")
+		fmt.Println("---------------------------------------------------------------------------------------------------")
+		fmt.Printf("PRIV_HEX=%s\n", hex)
+		fmt.Printf("WALLET_ADDR=%s\n", Addr.Hex())
+		fmt.Println("---------------------------------------------------------------------------------------------------")
+
+		// load the private
+	} else {
+		crypto.PrivateKeyHex = os.Getenv("PRIV_HEX")
+		_, err := crypto.LoadPrivateKeyAndAddr(crypto.PrivateKeyHex)
+		if err != nil {
+			log.Fatalln("Failed to load private key")
+			return
+		}
+		log.Default().Println("Private key and Address loaded")
+		fmt.Println("---------------------------------------------------------------------------------------------------")
+		fmt.Println("Private key: ", crypto.PrivateKeyHex)
+		fmt.Println("Wallet Address: ", crypto.Address.Hex())
+		fmt.Println("---------------------------------------------------------------------------------------------------")
 	}
 }
