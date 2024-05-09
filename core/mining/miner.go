f0bbc97f9fceeb7953bfd2e7cc20ac80df64b5e4
Shrey <shrey1patel2@gmail.com>
2024-05-10 05:03:30 +0530

    Update miner.go to fix the condition in Timer function

--- core/mining/miner.go
+++ core/mining/miner.go
@@ -161,7 +161,7 @@ func MiningInit(expectedMiner chan string, peerAddrs *[]string) { // add transac
 
 func Timer(timerCh chan string, miners *[]string) {
 	for {
-		if len(*miners) > 2 {
+		if len(*miners) > 1 {
 			break
 		}
 	}
